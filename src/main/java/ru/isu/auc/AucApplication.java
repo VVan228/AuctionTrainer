1a5874bcbd81ecf41366a81104a8204b9c7512c0
Leongard <vovchick2210@gmail.com>
2023-05-08 19:21:08 +0800

    init

---
+++ src/main/java/ru/isu/auc/AucApplication.java
@@ -0,0 +1,13 @@
+package ru.isu.auc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AucApplication.class, args);
+	}
+
+}
