daf28ea01606e69ce69547181131d4988b7032f2
Leongard <vovchick2210@gmail.com>
2025-02-24 17:13:34 +0800

    docker db fix19

--- db/setupdb.sh
+++ db/setupdb.sh
@@ -3,5 +3,5 @@ set -e
 # mysql.server start
 mysqld --initialize
 mysqld
-mysql -u root auction_demo_scheduler < ./auction_demo_scheduler.sql
+# mysql -u root auction_demo_scheduler < ./auction_demo_scheduler.sql
 mysqladmin -u root shutdown
