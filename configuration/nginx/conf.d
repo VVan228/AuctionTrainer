fe25b8e04825ab8b6744096a49c4df2dfc53d2ed
Leongard <vovchick2210@gmail.com>
2025-02-24 16:14:13 +0800

    docker db

--- configuration/nginx/conf.d
+++
@@ -1,7 +0,0 @@
-server { 
-	listen 80 default; 
-	root /var/www/html; 
-	location / { 
-		try_files /index.html /index.html /index.html =404;
-	}     
-} 
---
+++ db/setupdb.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+service mysql start
+mysql < ./auction_demo_scheduler.sql
+service mysql stop
