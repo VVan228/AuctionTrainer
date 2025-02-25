02a89c14d9929f4ffe6fdec0999ac71d7d8531c4
Leongard <vovchick2210@gmail.com>
2025-02-25 17:55:39 +0800

    new db

--- db/auction_demo.sql
+++ db/auction_demo.sql
@@ -16,12 +16,34 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `auction_demo`
+-- Table structure for table `connected_users`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `auction_demo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+DROP TABLE IF EXISTS `connected_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `connected_users` (
+  `room_id` bigint NOT NULL,
+  PRIMARY KEY (`room_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-USE `auction_demo`;
+--
+-- Table structure for table `connected_users_joined_users`
+--
+
+DROP TABLE IF EXISTS `connected_users_joined_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `connected_users_joined_users` (
+  `connected_users_room_id` bigint NOT NULL,
+  `joined_users_id` bigint NOT NULL,
+  KEY `FK6nlmo5169fpgvgtilq4hxbhem` (`joined_users_id`),
+  KEY `FKssdn140kktq7qk06wb44upd61` (`connected_users_room_id`),
+  CONSTRAINT `FK6nlmo5169fpgvgtilq4hxbhem` FOREIGN KEY (`joined_users_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FKssdn140kktq7qk06wb44upd61` FOREIGN KEY (`connected_users_room_id`) REFERENCES `connected_users` (`room_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `interval`
@@ -32,23 +54,15 @@ DROP TABLE IF EXISTS `interval`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `interval` (
   `id` bigint NOT NULL AUTO_INCREMENT,
+  `autoend` bit(1) DEFAULT NULL,
   `autostart` bit(1) DEFAULT NULL,
   `duration` bigint DEFAULT NULL,
-  `entity_id` bigint DEFAULT NULL,
+  `entity_uid` binary(16) DEFAULT NULL,
   `status` smallint DEFAULT NULL,
   `type` smallint DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=479 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `interval`
---
-
-LOCK TABLES `interval` WRITE;
-/*!40000 ALTER TABLE `interval` DISABLE KEYS */;
-/*!40000 ALTER TABLE `interval` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `interval_intervals`
@@ -68,15 +82,6 @@ CREATE TABLE `interval_intervals` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `interval_intervals`
---
-
-LOCK TABLES `interval_intervals` WRITE;
-/*!40000 ALTER TABLE `interval_intervals` DISABLE KEYS */;
-/*!40000 ALTER TABLE `interval_intervals` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `interval_point`
 --
 
@@ -85,19 +90,15 @@ DROP TABLE IF EXISTS `interval_point`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `interval_point` (
   `id` bigint NOT NULL AUTO_INCREMENT,
+  `at_least_one_auto_end` bit(1) DEFAULT NULL,
+  `at_least_one_auto_start` bit(1) DEFAULT NULL,
+  `at_least_one_manual_end` bit(1) DEFAULT NULL,
+  `at_least_one_manual_start` bit(1) DEFAULT NULL,
   `queue_id` bigint DEFAULT NULL,
+  `timestamp` bigint DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=409 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `interval_point`
---
-
-LOCK TABLES `interval_point` WRITE;
-/*!40000 ALTER TABLE `interval_point` DISABLE KEYS */;
-/*!40000 ALTER TABLE `interval_point` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `interval_point_interval_end_ids`
@@ -108,20 +109,12 @@ DROP TABLE IF EXISTS `interval_point_interval_end_ids`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `interval_point_interval_end_ids` (
   `interval_point_id` bigint NOT NULL,
-  `interval_end_ids` bigint DEFAULT NULL,
+  `autoend` bit(1) DEFAULT NULL,
+  `interval_id` bigint DEFAULT NULL,
   KEY `FKibx29xqm1bhsrx6edhy6su1lx` (`interval_point_id`),
   CONSTRAINT `FKibx29xqm1bhsrx6edhy6su1lx` FOREIGN KEY (`interval_point_id`) REFERENCES `interval_point` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `interval_point_interval_end_ids`
---
-
-LOCK TABLES `interval_point_interval_end_ids` WRITE;
-/*!40000 ALTER TABLE `interval_point_interval_end_ids` DISABLE KEYS */;
-/*!40000 ALTER TABLE `interval_point_interval_end_ids` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `interval_point_interval_start_ids`
@@ -132,20 +125,12 @@ DROP TABLE IF EXISTS `interval_point_interval_start_ids`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `interval_point_interval_start_ids` (
   `interval_point_id` bigint NOT NULL,
-  `interval_start_ids` bigint DEFAULT NULL,
+  `autostart` bit(1) DEFAULT NULL,
+  `interval_id` bigint DEFAULT NULL,
   KEY `FKtj1xg8pkc53acb44et7r94di6` (`interval_point_id`),
   CONSTRAINT `FKtj1xg8pkc53acb44et7r94di6` FOREIGN KEY (`interval_point_id`) REFERENCES `interval_point` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `interval_point_interval_start_ids`
---
-
-LOCK TABLES `interval_point_interval_start_ids` WRITE;
-/*!40000 ALTER TABLE `interval_point_interval_start_ids` DISABLE KEYS */;
-/*!40000 ALTER TABLE `interval_point_interval_start_ids` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `interval_queue`
@@ -157,19 +142,10 @@ DROP TABLE IF EXISTS `interval_queue`;
 CREATE TABLE `interval_queue` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `current_index` bigint DEFAULT NULL,
-  `room_id` bigint DEFAULT NULL,
+  `room_uid` binary(16) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `interval_queue`
---
-
-LOCK TABLES `interval_queue` WRITE;
-/*!40000 ALTER TABLE `interval_queue` DISABLE KEYS */;
-/*!40000 ALTER TABLE `interval_queue` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `interval_queue_interval_points`
@@ -189,15 +165,6 @@ CREATE TABLE `interval_queue_interval_points` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `interval_queue_interval_points`
---
-
-LOCK TABLES `interval_queue_interval_points` WRITE;
-/*!40000 ALTER TABLE `interval_queue_interval_points` DISABLE KEYS */;
-/*!40000 ALTER TABLE `interval_queue_interval_points` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `lot`
 --
 
@@ -206,48 +173,38 @@ DROP TABLE IF EXISTS `lot`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `lot` (
   `id` bigint NOT NULL AUTO_INCREMENT,
+  `uid` binary(16) DEFAULT NULL,
   `end_on_all_answered` bit(1) DEFAULT NULL,
   `limit_sum` int DEFAULT NULL,
   `min_bet_step` int DEFAULT NULL,
   `start_sum` int DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `sum` bigint DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKnhk8321kn0ly9dscl849c30wi` (`user_id`),
+  CONSTRAINT `FKnhk8321kn0ly9dscl849c30wi` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=268 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `lot`
+-- Table structure for table `lot_bets`
 --
 
-LOCK TABLES `lot` WRITE;
-/*!40000 ALTER TABLE `lot` DISABLE KEYS */;
-/*!40000 ALTER TABLE `lot` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `notification`
---
-
-DROP TABLE IF EXISTS `notification`;
+DROP TABLE IF EXISTS `lot_bets`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `notification` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `payload` json DEFAULT NULL,
-  `type` smallint DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `lot_bets` (
+  `lot_id` bigint NOT NULL,
+  `sum` bigint DEFAULT NULL,
+  `user_id` bigint DEFAULT NULL,
+  KEY `FKjxjwig9pg6pegbv3y9sdjd0b` (`user_id`),
+  KEY `FKkkm4r01h33ipmnpukcju51eg0` (`lot_id`),
+  CONSTRAINT `FKjxjwig9pg6pegbv3y9sdjd0b` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FKkkm4r01h33ipmnpukcju51eg0` FOREIGN KEY (`lot_id`) REFERENCES `lot` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `notification`
---
-
-LOCK TABLES `notification` WRITE;
-/*!40000 ALTER TABLE `notification` DISABLE KEYS */;
-/*!40000 ALTER TABLE `notification` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `room`
@@ -258,23 +215,16 @@ DROP TABLE IF EXISTS `room`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `room` (
   `id` bigint NOT NULL AUTO_INCREMENT,
+  `uid` binary(16) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
+  `room_type` smallint DEFAULT NULL,
   `start_time` datetime(6) DEFAULT NULL,
   `creator_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FKtl14kvxhp8bam5wu4f1oqn3gs` (`creator_id`),
   CONSTRAINT `FKtl14kvxhp8bam5wu4f1oqn3gs` FOREIGN KEY (`creator_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `room`
---
-
-LOCK TABLES `room` WRITE;
-/*!40000 ALTER TABLE `room` DISABLE KEYS */;
-/*!40000 ALTER TABLE `room` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `room_intervals`
@@ -294,15 +244,6 @@ CREATE TABLE `room_intervals` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `room_intervals`
---
-
-LOCK TABLES `room_intervals` WRITE;
-/*!40000 ALTER TABLE `room_intervals` DISABLE KEYS */;
-/*!40000 ALTER TABLE `room_intervals` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `round`
 --
 
@@ -311,50 +252,152 @@ DROP TABLE IF EXISTS `round`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `round` (
   `id` bigint NOT NULL AUTO_INCREMENT,
+  `uid` binary(16) DEFAULT NULL,
   `ascending` bit(1) DEFAULT NULL,
-  `room_id` bigint DEFAULT NULL,
+  `room_uid` binary(16) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=151 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `round`
+-- Table structure for table `template`
 --
 
-LOCK TABLES `round` WRITE;
-/*!40000 ALTER TABLE `round` DISABLE KEYS */;
-/*!40000 ALTER TABLE `round` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `team`
---
-
-DROP TABLE IF EXISTS `team`;
+DROP TABLE IF EXISTS `template`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `team` (
+CREATE TABLE `template` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `captain_id` bigint DEFAULT NULL,
+  `approves_amount` int DEFAULT NULL,
+  `creation_time` datetime(6) DEFAULT NULL,
+  `end_on_all_answered` bit(1) DEFAULT NULL,
+  `limit_sum` int DEFAULT NULL,
+  `min_bet_step` int DEFAULT NULL,
+  `start_sum` int DEFAULT NULL,
+  `lot_duration` bigint DEFAULT NULL,
+  `lot_pause_duration` bigint DEFAULT NULL,
+  `round_pause_duration` bigint DEFAULT NULL,
+  `template_name` varchar(255) DEFAULT NULL,
+  `is_default` bit(1) DEFAULT NULL,
+  `is_private` bit(1) DEFAULT NULL,
   `creator_id` bigint DEFAULT NULL,
+  `manual_mode` bit(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FKmy5ix43udwyht8c2diwt4cyua` (`captain_id`),
-  KEY `FK16hyjup1jsh1sr9x3q1pm3rhl` (`creator_id`),
-  CONSTRAINT `FK16hyjup1jsh1sr9x3q1pm3rhl` FOREIGN KEY (`creator_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `FKmy5ix43udwyht8c2diwt4cyua` FOREIGN KEY (`captain_id`) REFERENCES `user` (`id`)
+  KEY `FKdxhgepg2pwv3kvccbm8w5pilm` (`creator_id`),
+  CONSTRAINT `FKdxhgepg2pwv3kvccbm8w5pilm` FOREIGN KEY (`creator_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=179 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `template_approval`
+--
+
+DROP TABLE IF EXISTS `template_approval`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `template_approval` (
+  `template_id` bigint NOT NULL,
+  PRIMARY KEY (`template_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `team`
+-- Table structure for table `template_approval_approvals`
 --
 
-LOCK TABLES `team` WRITE;
-/*!40000 ALTER TABLE `team` DISABLE KEYS */;
-/*!40000 ALTER TABLE `team` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `template_approval_approvals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `template_approval_approvals` (
+  `template_approval_template_id` bigint NOT NULL,
+  `approvals` bit(1) DEFAULT NULL,
+  `approvals_key` bigint NOT NULL,
+  PRIMARY KEY (`template_approval_template_id`,`approvals_key`),
+  CONSTRAINT `FK25yd891tg9gpukmntdgrs1cm2` FOREIGN KEY (`template_approval_template_id`) REFERENCES `template_approval` (`template_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `template_approval_user_ids`
+--
+
+DROP TABLE IF EXISTS `template_approval_user_ids`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `template_approval_user_ids` (
+  `template_approval_template_id` bigint NOT NULL,
+  `user_ids` bigint DEFAULT NULL,
+  KEY `FKennws7hae0j09y3dqeg2pa6np` (`template_approval_template_id`),
+  CONSTRAINT `FKennws7hae0j09y3dqeg2pa6np` FOREIGN KEY (`template_approval_template_id`) REFERENCES `template_approval` (`template_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `template_lot_descriptions`
+--
+
+DROP TABLE IF EXISTS `template_lot_descriptions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `template_lot_descriptions` (
+  `template_id` bigint NOT NULL,
+  `lot_descriptions` varchar(255) DEFAULT NULL,
+  KEY `FK651n2yw07uwmk8kc1x9nys255` (`template_id`),
+  CONSTRAINT `FK651n2yw07uwmk8kc1x9nys255` FOREIGN KEY (`template_id`) REFERENCES `template` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `template_lot_names`
+--
+
+DROP TABLE IF EXISTS `template_lot_names`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `template_lot_names` (
+  `template_id` bigint NOT NULL,
+  `lot_names` varchar(255) DEFAULT NULL,
+  KEY `FKgc9bnpayigfahyepfistnr8py` (`template_id`),
+  CONSTRAINT `FKgc9bnpayigfahyepfistnr8py` FOREIGN KEY (`template_id`) REFERENCES `template` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `template_round_types`
+--
+
+DROP TABLE IF EXISTS `template_round_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `template_round_types` (
+  `template_id` bigint NOT NULL,
+  `round_types` bit(1) DEFAULT NULL,
+  KEY `FK96a73ui6ib9t1i4p5tii9mdlq` (`template_id`),
+  CONSTRAINT `FK96a73ui6ib9t1i4p5tii9mdlq` FOREIGN KEY (`template_id`) REFERENCES `template` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `template_rounds`
+--
+
+DROP TABLE IF EXISTS `template_rounds`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `template_rounds` (
+  `template_id` bigint NOT NULL,
+  `end_on_all_answered` bit(1) DEFAULT NULL,
+  `limit_sum` int DEFAULT NULL,
+  `min_bet_step` int DEFAULT NULL,
+  `start_sum` int DEFAULT NULL,
+  `lot_duration` bigint DEFAULT NULL,
+  `lot_pause_duration` bigint DEFAULT NULL,
+  `round_pause_duration` bigint DEFAULT NULL,
+  `ascending` bit(1) DEFAULT NULL,
+  KEY `FKjqye7vx9j3fjc7rasmcqdre9w` (`template_id`),
+  CONSTRAINT `FKjqye7vx9j3fjc7rasmcqdre9w` FOREIGN KEY (`template_id`) REFERENCES `template` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `user`
@@ -371,17 +414,8 @@ CREATE TABLE `user` (
   `role` smallint DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -392,4 +426,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-11 18:55:49
+-- Dump completed on 2025-02-25 17:54:38
